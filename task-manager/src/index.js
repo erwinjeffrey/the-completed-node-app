110d9a90dc05ee6cd51d10157d673f42308bb1a7
System Administrator <root@Maxs-MacBook-Pro.local>
2020-05-06 16:51:42 -0400

    generating authentication tokens

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,17 +14,17 @@ app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
 
-const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const myFunction = async () => {
-  const password = 'Red12345!';
-  const hashedPassword = await bcrypt.hash(password, 8);
+  const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', {
+    expiresIn: '7 days'
+  });
+  console.log(token);
 
-  console.log(password);
-  console.log(hashedPassword);
+  const data = jwt.verify(token, 'thisismynewcourse');
 
-  const isMatch = await bcrypt.compare('red12345!', hashedPassword);
-  console.log(isMatch);
+  console.log(data);
 };
 
 myFunction();
