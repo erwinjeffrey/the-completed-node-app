ce731cb2c89f2e74c874402116a6d89175aefa67
System Administrator <root@Maxs-MacBook-Pro.local>
2020-05-06 17:15:25 -0400

    practicing middleware

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -5,7 +5,18 @@ const taskRoouter = require('./routes/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
+//middleware
+/*app.use((req, res, next) => {
+  if (req.method === 'GET') {
+    res.send('GET requests are disabled');
+  } else {
+    next();
+  }
+});*/
 
+app.use((req, res, next) => {
+  res.status(503).send('Site is currently down. Check back soon!');
+});
 app.use(express.json()); // parse json
 app.use(userRouter);
 app.use(taskRoouter);
