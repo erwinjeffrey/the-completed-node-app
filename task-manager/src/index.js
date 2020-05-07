146f4539c3da3d0868ae8334f0f7e26f9bd6a0d3
System Administrator <root@Maxs-MacBook-Pro.local>
2020-05-07 15:04:14 -0400

    the UserTask relationship

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -26,4 +26,17 @@ app.listen(port, () => {
   console.log('Server is up on port ' + port);
 });
 
+const Task = require('./models/task');
+const User = require('./models/user');
 
+const main = async () => {
+  // const task = await Task.findById('5eb4553a2cd1131816d367bd');
+  // await task.populate('owner').execPopulate();
+  // console.log(task.owner);
+
+  const user = await User.findById('5eb453ef3137391807a91305');
+  await user.populate('tasks').execPopulate();
+  console.log(user.tasks);
+};
+
+main();
