a87ee7be780998ca009af4c558b00fc21680ce4e
System Administrator <root@Maxs-MacBook-Pro.local>
2020-05-07 16:17:14 -0400

    Authenticating task endpoints

--- task-manager/src/routes/task.js
+++ task-manager/src/routes/task.js
@@ -16,22 +16,24 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
   try {
-    taks = await Task.find({});
-    res.send(taks);
+    //const tasks = await Task.find({ owner: req.user._id}) one solution
+    await req.user.populate('tasks').execPopulate();
+    res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
   }
 });
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
   const _id = req.params.id;
 
   try {
-    task = await Task.findById(_id);
+    //finding task by id and checking if it belong to logging owner
+    const task = await Task.findOne({ _id, owner: req.user._id });
     if (!task) {
-      return res.status(400).send();
+      return res.status(404).send();
     }
     res.send(task);
   } catch (e) {
@@ -39,7 +41,7 @@ router.get('/tasks/:id', async (req, res) => {
   }
 });
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['description', 'completed'];
   const isValidOperation = updates.every(update => {
@@ -50,26 +52,31 @@ router.patch('/tasks/:id', async (req, res) => {
     return res.status(400).send({ error: 'Invalid updates' });
   }
   try {
-    const task = await Task.findById(req.params.id);
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
 
     updates.forEach(update => (task[update] = req.body[update]));
     await task.save();
-
-    if (!task) {
-      return res.status(400).send();
-    }
     res.send(task);
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id
+    });
     if (!task) {
-      return res.status(400).send();
+      return res.status(404).send();
     }
 
     res.send(task);
