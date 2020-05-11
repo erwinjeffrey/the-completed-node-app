170a446bd607196a611744b9f6134332db5062a7
System Administrator <root@Maxs-MacBook-Pro.local>
2020-05-11 16:08:43 -0400

    Handling express errors

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -21,10 +21,16 @@ const upload = multer({
     cb(undefined, true);
   }
 });
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-  res.send();
-});
+app.post(
+  '/upload',
+  upload.single('upload'),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 app.use(express.json()); // parse json
 app.use(userRouter);
