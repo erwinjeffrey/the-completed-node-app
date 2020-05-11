811cfd30118acb8c3e93b974a49a380de2980c07
System Administrator <root@Maxs-MacBook-Pro.local>
2020-05-11 15:39:59 -0400

    validating file upload

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -9,7 +9,17 @@ const port = process.env.PORT || 3000;
 //example of files uploading
 const multer = require('multer');
 const upload = multer({
-  dest: 'images'
+  dest: 'images',
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.endsWith('.pdf')) {
+      return cb(new Error('Please upload a PDF'));
+    }
+
+    cb(undefined, true);
+  }
 });
 
 app.post('/upload', upload.single('upload'), (req, res) => {
