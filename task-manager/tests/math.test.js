bc11db1eead39f8ab265f95491b9b456a4ab1cb5
System Administrator <root@Maxs-MacBook-Pro.local>
2020-05-13 10:57:45 -0400

    Writing tests and Assertions

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,5 +1,12 @@
-test('Hello world!', () => {});
+const { calculateTip } = require('../src/math');
 
-test('This should fail', () => {
-  throw new Error('Faillure');
+test('Should calculate total with tip', () => {
+  const total = calculateTip(10, 0.3);
+
+  expect(total).toBe(13);
+});
+
+test('should calculate total with default tip', () => {
+  const total = calculateTip(10);
+  expect(total).toBe(12.5);
 });
