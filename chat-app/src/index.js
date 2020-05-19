c44ce0752cbc660876d4542cf661765ee8b66a89
System Administrator <root@Maxs-MacBook-Pro.local>
2020-05-19 11:53:46 -0400

    Sending messages to rooms

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -34,29 +34,35 @@ io.on('connection', socket => {
 
     socket.join(user.room);
 
-    socket.emit('message', generateMessage('Welcome!'));
+    socket.emit('message', generateMessage('Admin', 'Welcome!'));
     socket.broadcast
       .to(user.room)
-      .emit('message', generateMessage(`${user.username} has joined!`));
+      .emit(
+        'message',
+        generateMessage('Admin', `${user.username} has joined!`)
+      );
 
     callback();
   });
 
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter();
+    const user = getUser(socket.id);
 
     if (filter.isProfane(message)) {
       return callback('Profanity is not allowed!');
     }
 
-    io.to('boca chica').emit('message', generateMessage(message));
+    io.to(user.room).emit('message', generateMessage(user.username, message));
     callback();
   });
 
   socket.on('sendLocation', (coords, callback) => {
-    io.emit(
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
       'locationMessage',
       generateLocationMessage(
+        user.username,
         `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
       )
     );
@@ -69,7 +75,7 @@ io.on('connection', socket => {
     if (user) {
       io.to(user.room).emit(
         'message',
-        generateMessage(`${user.username} has left!`)
+        generateMessage('Admin', `${user.username}, has left!`)
       );
     }
   });
